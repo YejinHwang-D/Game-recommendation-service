9c5f229d34a7583ae79a8aeec33a240a198d9d2f
Bucky6683 <bucky5683@gmail.com>
2022-01-15 16:54:57 +0900

    api 받아옴

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const ranking = document.getElementsByClassName('rankingArea')
 const gamesinfoarea = document.getElementsByClassName('games')
 const gameinfo = document.getElementsByClassName('game')
@@ -5,11 +7,30 @@ const game_rank = document.getElementsByClassName('rank')
 const game_icon = document.getElementsByClassName('app-image')
 const game_name = document.getElementsByClassName('gamename')
 
-//api 예시: http://www.grac.or.kr/WebService/GameSearchSvc.asmx/game?gametitle=[게임물명]&entname=&rateno=&display=10&pageno=1
-let apiUrl = "http://www.grac.or.kr/WebService/GameSearchSvc.asmx/game?"
+//api 예시
+//GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
+//GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
 
-//list api
-let listapiURL = "https://api.rawg.io/api/games"
+const api_Key = "3c4db8d9f2fb4602938c65f41f06df0b";
+
+let apiUrlset = "https://api.rawg.io/api/"
+const apigamelist = `https://api.rawg.io/api/games?key=${api_Key}&`
+
+const getgameList = async () => {
+    const resp = await fetch(apigamelist, {
+        mod: 'crs',
+        method: 'GET',
+        crossDomain: true,
+        headers:{
+            'User-Agent': 'game-recommand-service'
+        }
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data.results;
+}
 
 function showRanking(){
 }
+
+getgameList();
